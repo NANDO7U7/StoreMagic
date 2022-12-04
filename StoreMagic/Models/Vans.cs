740ed9df11d8857fdedd9055f5c6e3bb9640175c
FERNANDO JOSE CRUZ CHEVEZ <SMSS141122@ugb.edu.sv>
2022-12-03 23:21:12 -0600

    saaaaaaaaa

---
+++ StoreMagic/Models/Vans.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreMagic.Models
+{
+    public class Vans
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
